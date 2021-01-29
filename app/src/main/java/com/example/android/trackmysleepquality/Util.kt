99391aba6323a7cbba86cd01d62ffb58d4f1bcce
Aman Raj <amanrj2914@gmail.com>
2021-01-29 16:27:29 +0530

    updated the adapter, displays the sleep duration, quality and image.

--- app/src/main/java/com/example/android/trackmysleepquality/Util.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/Util.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.android.trackmysleepquality
 
 import android.annotation.SuppressLint
@@ -5,11 +21,35 @@ import android.content.res.Resources
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
-import android.widget.TextView
-import androidx.core.text.HtmlCompat
-import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 import java.text.SimpleDateFormat
+import java.util.concurrent.TimeUnit
+import java.util.*
+
+
+private val ONE_MINUTE_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.MINUTES)
+private val ONE_HOUR_MILLIS = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS)
+
+fun convertDurationToFormatted(startTimeMilli: Long, endTimeMilli: Long, res: Resources): String {
+    val durationMilli = endTimeMilli - startTimeMilli
+    val weekdayString = SimpleDateFormat("EEEE", Locale.getDefault()).format(startTimeMilli)
+    return when {
+        durationMilli < ONE_MINUTE_MILLIS -> {
+            val seconds = TimeUnit.SECONDS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.seconds_length, seconds, weekdayString)
+        }
+        durationMilli < ONE_HOUR_MILLIS -> {
+            val minutes = TimeUnit.MINUTES.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.minutes_length, minutes, weekdayString)
+        }
+        else -> {
+            val hours = TimeUnit.HOURS.convert(durationMilli, TimeUnit.MILLISECONDS)
+            res.getString(R.string.hours_length, hours, weekdayString)
+        }
+    }
+}
+
+
 
 /**
  * These functions create a formatted string that can be set in a TextView.
@@ -37,7 +77,7 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
  * and convert it to a nicely formatted string for display.
  *
  * EEEE - Display the long letter version of the weekday
- * MMM - Display the letter abbreviation of the month
+ * MMM - Display the letter abbreviation of the nmotny
  * dd-yyyy - day in month and full year numerically
  * HH:mm - Hours and minutes in 24hr format
  */
@@ -83,11 +123,9 @@ fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
             }
         }
     }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
-    } else {
-        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
-    }
+    // fromHtml is deprecated for target API without a flag, but since our minSDK is 19, we
+    // can't use the newer version, which requires minSDK of 24
+    //https://developer.android.com/reference/android/text/Html#fromHtml(java.lang.String,%20int)
+    return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
 }
 
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
